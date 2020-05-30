9c05cd4ddf852d14ed8fe27ba02880dc4980e16f
Charles Best <charlesbest8@gmail.com>
2020-05-30 17:51:26 +0100

    Updates

---
+++ precache-manifest.9c7c01d238d91877de56f76f1d363c09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b8ee70ebb5b11f3ab379bd3c5b1020",
+    "url": "/Best-Headlines/index.html"
+  },
+  {
+    "revision": "0ecf679b1669b06da47c",
+    "url": "/Best-Headlines/static/css/main.8234e966.chunk.css"
+  },
+  {
+    "revision": "0b5cd4d0e0018c64d4c8",
+    "url": "/Best-Headlines/static/js/2.38d1eaac.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Best-Headlines/static/js/2.38d1eaac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ecf679b1669b06da47c",
+    "url": "/Best-Headlines/static/js/main.1d5e975c.chunk.js"
+  },
+  {
+    "revision": "3597e836f67daf9e1753",
+    "url": "/Best-Headlines/static/js/runtime-main.4b87957c.js"
+  }
+]);
